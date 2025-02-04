a88708200495595dea5ef3b5e1f85d0a32ce3f17
Shwetakshi Priya <110232435+shwetakshipriya@users.noreply.github.com>
2025-02-05 00:58:41 +0530

    build changes

--- my_portfolio/src/components/HeroSection.jsx
+++ my_portfolio/src/components/HeroSection.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 
-
 const HeroSection = () => {
   return (
     <section>
@@ -11,9 +10,9 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
-            <br></br>
+            <br />
             <TypeAnimation
               sequence={[
                 "Shwetakshi",
@@ -30,17 +29,21 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7bE]  text-base sm:text-lg  mb-6 lg:text-xl ">
-            I am a full stack developer and this is my portfolio I am also a
+          <p className="text-[#ADB7bE] text-base sm:text-lg mb-6 lg:text-xl">
+            I am a full stack developer and this is my portfolio. I am also a
             student at the University and I am currently studying Computer
-            Science
+            Science.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500  to-pink-500 hover:bg-slate-200 text-white">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
               Hire Me
             </button>
 
-            <a  href = "https://drive.google.com/file/d/1E787qgseMch9BKpJf9qp5FbXeyAEqDY5/view?usp=sharing" className="inline-flex px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border mt-3" download>
+            <a
+              href="https://drive.google.com/file/d/1E787qgseMch9BKpJf9qp5FbXeyAEqDY5/view?usp=sharing"
+              className="inline-flex px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border mt-3"
+              download
+            >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
@@ -54,7 +57,7 @@ const HeroSection = () => {
               alt="hero image"
               width={300}
               height={300}
-              className="absolute transform  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             />
           </div>
         </div>
